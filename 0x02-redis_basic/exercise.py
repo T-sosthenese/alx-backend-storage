3d912a3bdef878ad002e6c6b28cb3178fc385e68
T-sosthenese <sosthenetimi94@gmail.com>
2023-12-20 15:31:45 +0000

    Redis replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -55,7 +55,8 @@ def replay(method: Callable) -> None:
     cache = redis.Redis()
     calls = cache.get(name).decode("utf-8")
     print("{} was called {} times:".format(name, calls))
-    inputs = cache.lrange(name + ":outputs", 0, -1)
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
     for i, o in zip(inputs, outputs):
         print("{}(*{}) -> {}".format(name, i.decode('utf-8'),
                                      o.decode('utf-8')))
