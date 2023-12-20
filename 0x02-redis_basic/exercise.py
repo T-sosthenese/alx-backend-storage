0ceb96993bf3cc75055db336aee4dcb8e5e07225
T-sosthenese <sosthenetimi94@gmail.com>
2023-12-20 12:26:45 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ Storing data with Redis
 import uuid
 from typing import Union
 import redis
+from typing import Callable
 
 
 class Cache:
@@ -26,3 +27,34 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self,
+        key: str,
+        fn: Callable = None
+    ) -> Union[str, bytes, int, float]:
+        """
+        A method that takes any key string argument and an optional callable
+        function fn. The callable is used to convert data back to the desired
+        format. It conserves the original Redis.get behavior if the key does
+        not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        This method retrieves data associated with a given key from Redis
+        and decodes it from bytes to a string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves data and converts it to an integer
+        """
+        return self.get(key, fn=int)
